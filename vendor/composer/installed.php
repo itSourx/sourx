8ccf9615b0815c3395c7956ffd2c524d242c42e8
bolaak <it@sourx.com>
2024-10-12 10:58:22 +0100

    composer.lock autoload.php

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-lab',
+        'version' => 'dev-lab',
+        'reference' => 'cc22754aa2ac0e2394b3e18378f1b7d1ec57ed95',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-lab',
+            'version' => 'dev-lab',
+            'reference' => 'cc22754aa2ac0e2394b3e18378f1b7d1ec57ed95',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

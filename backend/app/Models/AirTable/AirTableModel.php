958c378c86a3bfaa5205a40e7bd968b6c7ddbe75
bolaak <it@sourx.com>
2024-10-13 01:41:32 +0100

    correction des erreurs dans app et vendor

--- backend/app/Models/AirTable/AirTableModel.php
+++ backend/app/Models/AirTable/AirTableModel.php
@@ -2,36 +2,87 @@
 
 namespace App\Models\AirTable;
 
+
+use Illuminate\Database\Eloquent\Model;
+use Serializable;
+use Tapp\Airtable\Facades\AirtableFacade as Airtable;
 use Tapp\Airtable\Facades\AirtableFacade;
 
-
-class AirTableModel
+class AirtableModel
 {
-    protected $record;
-    protected $fields = "fields";
+    use AirtableTrait ;
+     // Remplacer par le nom de votre table Airtable
+    protected $id ;
+    protected $nom ;
+    protected $prenom ;
+    protected $role ;
+    protected $email ;
+    protected $telephone ;
+    protected $airtable ;
+    protected $table ;
 
-    public function __construct($record)
+    protected $fillable = [];
+    static protected $ok = 55;
+
+
+
+
+    public function __construct()
     {
-        $this->record = $record;
+        $airtable = new Airtable([
+            'base_id' => env('AIRTABLE_BASE_ID'),
+            'api_key' => env('AIRTABLE_KEYAIRTABLE_KEY'),
+        ]);
+        
+        $this->airtable = $airtable;
+        
     }
 
-    public function __get($key)
+    static public function all()
     {
-        // id
-        if ($key == "id") {
-            return $this->record['id'];
-        }
+        
+    }
 
-
-        // fields
-        if (array_key_exists($key, $this->record[$this->fields])) {
-            return $this->record[$this->fields][$key];
-        }
+    public static function find($id)
+    {
+        $cl = get_called_class();
+        $instance = new $cl ;
+        $table = $instance->table ;
+        $filable =  $instance->filable;
+        $reponse = AirtableFacade::table($table)->where("id",$id)->get();
+        return  AirtableTrait::fields($reponse,$filable) ;
         
 
         
-        return null;
+        
     }
 
+    public function create($data)
     
+    {
+        
+    }
+
+    //public function update($id, $data)
+    // {
+    //     $this->airtable->updateRecord('nom_de_la_table_Airtable', $id, $data);
+    // }
+
+    // public function delete($id)
+    // {
+    //     $this->airtable->deleteRecord('nom_de_la_table_Airtable', $id);
+    // }
+
+    public function __get($name) {
+        if (isset($this->data[0][$name])) {
+            $values = [];
+            foreach ($this->data as $row) {
+                $values[] = $row[$name];
+            }
+            return $values;
+        } else {
+            throw new \Exception("Propriété '$name' inexistante");
+        }
+    }
 }
+

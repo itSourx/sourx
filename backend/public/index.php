f5c4f3938038b3fba94b7f724b92179fd85c7e69
D1D0S <didosessou@gmail.com>
2024-10-15 17:56:13 +0100

    Refactor require statement for vendor/autoload.php path

--- backend/public/index.php
+++ backend/public/index.php
@@ -31,7 +31,8 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 |
 */
 
- require __DIR__.'/../vendor/autoload.php';
+//  require __DIR__.'/../vendor/autoload.php';
+require realpath(__DIR__.'/../vendor/autoload.php');
 /*
 |--------------------------------------------------------------------------
 | Run The Application

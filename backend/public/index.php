0b665b0b086d2eb26b931882cc02273c2637abb6
D1D0S <didosessou@gmail.com>
2024-10-15 12:48:01 +0100

    fix

--- backend/public/index.php
+++ backend/public/index.php
@@ -31,8 +31,7 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 |
 */
 
-// require __DIR__.'/../vendor/autoload.php';
-require __DIR__. '/vendor/autoload.php';
+ require __DIR__.'/../vendor/autoload.php';
 /*
 |--------------------------------------------------------------------------
 | Run The Application
